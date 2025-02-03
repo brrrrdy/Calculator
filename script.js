74918e2bffb5272091e8b4c43346630287bdb27a
Tom Alvarez <127635396+brrrrdy@users.noreply.github.com>
2025-02-03 13:47:57 +0100

    jest

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b)
+};
+
+const power = function(a, b) {
+	return Math.pow(a, b);
+};
+
+const factorial = function(a) {
+  if (a < 0) 
+    return -1;
+else if (a == 0) 
+  return 1;
+else {
+  return (a * factorial(a - 1));
+}
+};
+
 module.exports = {
     add,
     subtract,
--- script.test.js
+++ script.test.js
@@ -1,1 +1,7 @@
 const calculator = require('./calculator');
+
+describe('add', () => {
+    test('adds 0 and 0', () => {
+      expect(calculator.add(0, 0)).toBe(0);
+    });
+});
